006169e4a34b90f9d8124ca237d9b6565e75a6a8
amird308 <amird308@gmail.com>
2026-02-15 01:18:03 +0330

    Refactor admin JWT authentication by removing unnecessary options and simplifying configuration

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-    options: {
-      secure: false,
-    },
+    secret: env('ADMIN_JWT_SECRET')
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
--- config/plugins.js
+++ config/plugins.js
@@ -4,13 +4,6 @@ module.exports = ({ env }) => ({
       jwtSecret: env("JWT_SECRET"),
     },
   },
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-    options: {
-      expiresIn: '7d',
-      secure: true   // ❌ REMOVE THIS
-    }
-  },
   upload: {
     config: {
       provider: "local",
